9e957f223c07f4bb4326ffe6d7c2e1578a8fdf8c
AlexHwang <cksgh0316@gmail.com>
2021-12-30 22:55:31 +0900

    Initial Commit

---
+++ SeSAC_Farm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC_Farm
+//
+//  Created by AlexHwang on 2021/12/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
